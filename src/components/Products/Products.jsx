15e4d2f62dccdc6ae2c21e1021ac4bfe09fa246a
Maicon Ruas <maicon_ruas@hotmail.com>
2023-06-20 11:13:47 -0300

    Adicionado Provider

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useContext } from 'react';
 import fetchProducts from '../../api/fetchProduct';
 import './Products.css';
 import ProductCard from '../ProductCard/ProductCard';
 import Loading from '../Loading/Loading';
+import AppContext from '../../context/AppContext';
 
 function Products() {
 
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const {products, setProducts, loading, setLoading} = useContext(AppContext);
 
     useEffect(() => {
         fetchProducts('iphone').then((response) => {
