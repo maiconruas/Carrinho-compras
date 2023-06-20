f75cdf179bfbd8e3cdf0413561ec33559340a048
Maicon Ruas <maicon_ruas@hotmail.com>
2023-06-20 10:32:22 -0300

    Criação do Loading

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from 'react';
 import fetchProducts from '../../api/fetchProduct';
 import './Products.css';
 import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 function Products() {
 
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchProducts('iphone').then((response) => {
             setProducts(response);
+            setLoading(false);
         });
     }, []);
 
     return (
-        <section className="products container">
+        (loading && <Loading />) ||  <section className="products container">
             {products.map((product) => <ProductCard key={product.id} data={product} />)}<ProductCard data={{}} />
-        </section>
+        </section>  
     );
 }
 
